617ecdc183abee6d10c60b0e73b1ca4db2072fb8
Marco <marcoverbeek99@gmail.com>
2020-12-01 18:36:44 +0100

    Added POST /doctors w/ log

--- routes/doctorRouter.js
+++ routes/doctorRouter.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const doctor_controller = require("../controllers/doctorController")
 
 router.get("/doctors", doctor_controller.doctor_list)
+router.post("/doctors", doctor_controller.doctor_login)
 
 module.exports = router;
