7c9ed02746c7fe2a8c894574f9b70076e47568a0
JessMnk <58186217+JessMnk@users.noreply.github.com>
2020-12-03 01:46:44 +0100

    Created facility_login and facility_register method + added bcrypt dependency + mocha now only tests .js files

--- controllers/facilityController.js
+++ controllers/facilityController.js
@@ -1,4 +1,6 @@
 const Facility = require("../models/facility")
+const bcrypt = require('bcrypt')
+const SALT = 10
 
 /*
  * facility_list returns all facilities in db
@@ -10,9 +12,59 @@ exports.facility_list = function (req, res){
     })
 }
 
+function hash(password){
+    return bcrypt.hash(password, SALT)
+}
+
 /*
- * TODO
+ * facility_login verifies mail and password received with bcrypt
+ * Return: JSON of facility if authenticated, 401 otherwise
  */
 exports.facility_login = function(req, res){
-    console.log(req)
+     Facility.find({mail: req.body.mail}).then(result => {
+         if (result.length === 0) {
+             console.log("mail not in db")
+             res.sendStatus(401)
+         } else {
+             bcrypt.compare(req.body.password, result[0].password)
+                 .then(result => {
+                     console.log(result)
+                     if(!result) {
+                         console.log("wrong password")
+                         res.sendStatus(401)
+                     } else {
+                         console.log("authenticated !")
+                         res.json(result).status(200).end()
+                     }
+                })
+                 .catch(err => {
+                     if(process.env.NODE_ENV === "dev") console.error(err)
+                     res.sendStatus(500)
+                 })
+         }
+     })
 }
+
+exports.facility_register = function(req, res) {
+    hash(req.body.password)
+        .then(hash => {
+            console.log(hash)
+
+            const newFacility = new Facility({
+                name: req.body.name,
+                address: req.body.name,
+                mail: req.body.mail,
+                password: hash
+            })
+
+            Facility.collection.insertOne(newFacility)
+            res.sendStatus(200)
+        })
+        .catch(err => {
+            if(process.env.NODE_ENV === "dev") console.error(err)
+            res.sendStatus(500)
+        })
+
+
+}
+
--- routes/facilityRouter.js
+++ routes/facilityRouter.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const facility_controller = require("../controllers/facilityController")
 
 router.get("/facilities", facility_controller.facility_list)
-router.post("/facilities", facility_controller.facility_login)
+router.post("/facilities/login", facility_controller.facility_login)
+router.post("/facilities/register", facility_controller.facility_register)
 
 module.exports = router;
