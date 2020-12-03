2c5fd4a59af12c9208e1e597af652459cab0e92a
Marco <marcoverbeek99@gmail.com>
2020-12-03 21:48:25 +0100

    Updated POST on /qrcodes

--- controllers/qrCodeController.js
+++ controllers/qrCodeController.js
@@ -10,28 +10,21 @@ exports.qrcode_list = function (req, res){
     })
 }
 
-
 /*
  * qrcode_insert inserts a new qrcode in db
  * Return: 200 if success
  */
 exports.qrcode_insert = function(req, res) {
-    let qrcode
-    if(req.body.doctor_id == null){
-        qrcode = {location_id: req.body.location_id}
-    } else {
-        qrcode = {doctor_id: req.body.doctor_id}
-    }
-
-    new QrCode({qrcode})
-        .save()
-        .then(() => {
-            res.sendStatus(200)
-        })
-        .catch(err => {
-            if (process.env.NODE_ENV === "dev") console.error(err)
-            res.sendStatus(500)
-        })
-
-
+    new QrCode({
+        doctor_id: req.body.doctor_id ?? null,
+        location_id: req.body.location_id ?? null
+    })
+    .save()
+    .then(() => {
+        res.sendStatus(200)
+    })
+    .catch(err => {
+        if (process.env.NODE_ENV === "dev") console.error(err)
+        res.sendStatus(500)
+    })
 }
