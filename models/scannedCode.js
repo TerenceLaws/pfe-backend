344f58974eb7b65f1799e9c89256f2172c41de9f
JessMnk <58186217+JessMnk@users.noreply.github.com>
2020-12-08 16:32:24 +0100

    Renamed timestamp_enter && timestamp_exit in scannecodes schema to entry_date && exit_date

--- models/scannedCode.js
+++ models/scannedCode.js
@@ -3,8 +3,8 @@ const mongoose = require("../src/mongodb")
 const scannedCodeSchema = new mongoose.Schema({
     citizen_id: {type: mongoose.ObjectId, ref: 'Citizen', required: [true, 'a citizen\'s reference is required']},
     qrcode_id: {type: mongoose.ObjectId, ref: 'QrCode', required: [true, 'a qr code\'s reference is required']},
-    timestamp_enter: {type: Date, default: Date.now()},
-    timestamp_exit: {type: Date, default: null}
+    entry_date: {type: Date, default: Date.now()},
+    exit_date: {type: Date, default: null}
 })
 
 scannedCodeSchema.set('toJSON', {
