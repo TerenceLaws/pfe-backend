3a5dd1fa2bd162497e75848a29038e7c3f5bf194
Nicolas <nicolas.gerard@student.vinci.be>
2020-12-09 16:02:41 +0100

    put jwt token in array

--- controllers/professionalController.js
+++ controllers/professionalController.js
@@ -38,14 +38,14 @@ exports.professional_login = function(req, res){
                              for(let i=0; i<result.length; i++){
                                  result[i].password = undefined
                              }
-                             res.status(200).json({
-                                 result:result,
-                                 token : jwt.sign(
-                                     {id : result._id},
+                             res.status(200).json([{
+                                 result: result,
+                                 token: jwt.sign(
+                                     {id: result._id},
                                      "My_secret_jwt_token",
-                                     {expiresIn : '24h'}
-                                     )
-                             })
+                                     {expiresIn: '24h'}
+                                 )
+                             }])
                          }
                     })
                      .catch(err => {
