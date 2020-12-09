14f51fa5228a7ca6dddd5fa5ad238239178ca3b0
Nicolas <nicolas.gerard@student.vinci.be>
2020-12-09 16:44:43 +0100

    added middleware for decrypte jwt token

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, "My_secret_jwt_token");
+        const userId = decodedToken._id;
+        if (req.body.userId && req.body.userId !== userId) {
+            throw 'Invalid user ID';
+        } else {
+            next();
+        }
+    } catch {
+        res.status(401).json({
+            error: new Error('Invalid request!')
+        });
+    }
+};
--- routes/facilityLocationRouter.js
+++ routes/facilityLocationRouter.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router()
+const auth = require('../middleware/auth')
 
 const facility_location_controller = require("../controllers/locationController")
 
-router.get("/locations", facility_location_controller.location_list)
+router.get("/locations",auth, facility_location_controller.location_list)
 router.post("/locations", facility_location_controller.location_create)
 
 module.exports = router;
