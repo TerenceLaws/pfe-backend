b4fd61002b6384bcb4c5a63c2ce754dbc08cc0e8
Nicolas <nicolas.gerard@student.vinci.be>
2020-12-09 15:19:55 +0100

    changed jwt token

--- controllers/professionalController.js
+++ controllers/professionalController.js
@@ -38,7 +38,7 @@ exports.professional_login = function(req, res){
                              for(let i=0; i<result.length; i++){
                                  result[i].password = undefined
                              }
-                             res.status(200).set({"authorization": jwt.sign(
+                             res.status(200).set({"authorization": "Bearer "+jwt.sign(
                                      {id : result._id},
                                      "My_secret_jwt_token",
                                      {expiresIn : '24h'}
