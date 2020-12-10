127c0cf1638c5e5e7a4d6b66758726f956870185
Marco <marcoverbeek99@gmail.com>
2020-12-10 16:04:22 +0100

    Updated CORS setup

--- src/index.js
+++ src/index.js
@@ -10,23 +10,10 @@ app.use(express.json())
 app.use(logger('tiny', { skip: () => process.env.NODE_ENV === "tests" }))
 app.use(express.static('public'));
 
-app.use(cors())
+app.use(cors({
+    exposedHeaders: ['Content-Length', 'Authorization']
+}))
 app.use(bodyParser.json())
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-
-    // authorized headers for preflight requests
-    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,authorization');
-    next();
-
-    app.options('*', (req, res) => {
-        // allowed XHR methods
-        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
-        res.send();
-    });
-});
 
 const citizenRoutes = require("../routes/citizenRouter")
 const professionalRoutes = require("../routes/professionalRouter")
